2f8009b6b61d033843f7ebfee08616d7a1fcda61
sunminggang <942391953@qq.com>
2023-04-17 16:45:27 +0800

    chore: 添加commitlint配置

--- commitlint.config.ts
+++
@@ -1,8 +0,0 @@
-import type { UserConfig } from '@commitlint/types';
-// import { RuleConfigSeverity } from '@commitlint/types';
-
-const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional']
-};
-
-module.exports = Configuration;
