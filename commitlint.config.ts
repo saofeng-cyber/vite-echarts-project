d90564119804c43e98754049a66622fab1556e8d
sunminggang <942391953@qq.com>
2023-04-17 16:41:19 +0800

    chore: 添加commitlint配置

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,24 +1,8 @@
 import type { UserConfig } from '@commitlint/types';
-import { RuleConfigSeverity } from '@commitlint/types';
+// import { RuleConfigSeverity } from '@commitlint/types';
 
 const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
-  formatter: '@commitlint/format',
-  rules: {
-    'type-enum': [RuleConfigSeverity.Error, 'always', ['foo']]
-  },
-  ignores: [(commit) => commit === ''],
-  defaultIgnores: true,
-  helpUrl: 'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
-  prompt: {
-    messages: {},
-    questions: {
-      type: {
-        description: 'please input type:'
-      }
-    }
-  }
+  extends: ['@commitlint/config-conventional']
 };
 
 module.exports = Configuration;
