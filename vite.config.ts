8ae71d7bf654611c692e288f5fc670e9037eaaaf
sunminggang <942391953@qq.com>
2023-04-17 16:03:14 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 // import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import { resolve } from 'node:path'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import { resolve } from 'node:path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,11 +32,11 @@ export default defineConfig({
       resolvers: [NaiveUiResolver()]
     }),
     createSvgIconsPlugin({
-      iconDirs: [resolve(process.cwd(), "src/assets/icons")],
-      symbolId: "icon-[dir]-[name]",
-      inject: "body-last",
-      customDomId: "__svg__icons__dom__",
-  }),
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]',
+      inject: 'body-last',
+      customDomId: '__svg__icons__dom__'
+    })
   ],
   resolve: {
     alias: {
@@ -59,12 +59,7 @@ export default defineConfig({
     assetsDir: './',
     emptyOutDir: true,
     sourcemap: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
-      },
-    },
+    minify: 'esbuild',
     copyPublicDir: true,
     reportCompressedSize: true,
     chunkSizeWarningLimit: 500,
@@ -76,9 +71,7 @@ export default defineConfig({
         entryFileNames: 'js/[name]-[hash:10].js',
         assetFileNames: 'css/[name]-[hash:10].[ext]',
         compact: true
-      },
+      }
     }
   }
-})
-
-
+});
